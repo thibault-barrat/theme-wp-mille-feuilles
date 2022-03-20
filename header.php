10126e1febb76a28d57880bbd98ea47d8d2c7395
thibault-barrat <contact@thibault-barrat.com>
2022-03-20 12:21:20 +0100

    add shopping cart to header

--- header.php
+++ header.php
@@ -9,17 +9,6 @@
 
 <body <?php body_class(); ?>>
   <header>
-    <!-- <div class="site-search">
-  <?php echo do_shortcode('[fibosearch]'); ?>
-  </div>
-  <ul id="site-header-cart" class="site-header-cart menu">
-			<li>
-      <a class="cart-contents" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php esc_attr_e('View your shopping cart', 'storefront'); ?>">
-				<?php /* translators: %d: number of items in cart */ ?>
-				<?php echo wp_kses_post(WC()->cart->get_cart_subtotal()); ?> <span class="count"><?php echo wp_kses_data(sprintf(_n('%d item', '%d items', WC()->cart->get_cart_contents_count(), 'storefront'), WC()->cart->get_cart_contents_count())); ?></span>
-			</a>
-			</li>
-		</ul> -->
     <div class="container">
       <?php
       if (function_exists('the_custom_logo')) {
@@ -51,7 +40,13 @@
         ]);
         ?>
       </nav>
-      <i class="fa-solid fa-cart-shopping"></i>
+      <div class="header-cart">
+      
+      <a class="cart-contents" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="Voir votre panier">
+      <i class="fa-solid fa-basket-shopping"></i>
+				<?php echo wp_kses_post(WC()->cart->get_cart_subtotal()); ?> <span class="count"><?php echo wp_kses_data(sprintf(_n('%d livre', '%d livres', WC()->cart->get_cart_contents_count(), 'storefront'), WC()->cart->get_cart_contents_count())); ?></span>
+			</a>
+      </div>
       <button class="hamburger" type="button" aria-expanded="false" aria-controls="navigation" aria-label="menu">
         <span></span>
         <span></span>
