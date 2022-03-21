e57fdfc5ca695fd7ea4a44d256931ddd0f4acda0
thibault-barrat <contact@thibault-barrat.com>
2022-03-21 10:02:54 +0100

    add events section on homepage

--- front-page.php
+++ front-page.php
@@ -1,13 +1,22 @@
 <?php get_header() ?>
 <main>
-  <?php $sup_banner = get_field('sup_banner'); ?>
   <section class="banner">
+    <?php $sup_banner = get_field('sup_banner'); ?>
     <div class="banner__background">
       <img class="banner__background" src="<?php echo esc_url($sup_banner['background']['url']); ?>" alt="" />
     </div>
     <div class="container">
       <h1 class="banner__title"><?php echo esc_html($sup_banner['left_text']); ?></h1>
       <h2 class="banner__subtitle"><?php echo esc_html($sup_banner['right_text']); ?></h2>
+    </div>
+  </section>
+  <section class="events">
+    <?php $events = get_field('events'); ?>
+    <div class="container">
+      <h2 class="events__title"><?php echo esc_html($events['title']); ?></h2>
+      <div class="events__content">
+        <?php echo wp_kses_post($events['content']); ?>
+      </div>
     </div>
   </section>
 </main>
