a9586a5c81e08193e8f13a80255801aa5a1b9046
thibault-barrat <contact@thibault-barrat.com>
2022-03-28 11:07:01 +0200

    add image to events section

--- front-page.php
+++ front-page.php
@@ -7,15 +7,30 @@
     </div>
     <div class="container">
       <h1 class="banner__title"><?php echo esc_html($sup_banner['left_text']); ?></h1>
-      <h2 class="banner__subtitle"><?php echo esc_html($sup_banner['right_text']); ?></h2>
+      <h2 class="banner__title banner__title--small"><?php echo esc_html($sup_banner['right_text']); ?></h2>
     </div>
   </section>
   <section class="events">
     <?php $events = get_field('events'); ?>
     <div class="container">
-      <h2 class="events__title"><?php echo esc_html($events['title']); ?></h2>
-      <div class="events__content">
-        <?php echo wp_kses_post($events['content']); ?>
+      <h2 class="events__title homepage-section-title"><?php echo esc_html($events['title']); ?></h2>
+      <div class="events__row">
+        <div class="events__image">
+          <img src="<?php echo esc_url($events['image']['url']); ?>" alt="" />
+        </div>
+        <div class="events__content">
+          <?php echo wp_kses_post($events['content']); ?>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <section class="book-focus">
+    <?php $book_focus = get_field('book_focus'); ?>
+    <div class="container">
+      <h2 class="book-focus__title homepage-section-title"><?php echo esc_html($book_focus['title']); ?></h2>
+      <div class="book-focus__content">
+        <?php echo do_shortcode('[products orderby="rand" visibility="featured"]'); ?>
       </div>
     </div>
   </section>
