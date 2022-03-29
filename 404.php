28b5dbf8881e79175dba4363fde0f21b8109c721
thibault-barrat <contact@thibault-barrat.com>
2022-03-29 11:54:58 +0200

    add 404 temaplte

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+<main>
+<div class="container">
+  <h1>La page demandée est introuvable</h1>
+  <a href="<?php echo home_url(); ?>">Retour à l'accueil</a>
+</div>
+</main>
+<?php get_footer() ?>
