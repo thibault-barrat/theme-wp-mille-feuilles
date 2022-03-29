5c07c05559a154d305d7bde506e89c4cc669d001
thibault-barrat <contact@thibault-barrat.com>
2022-03-29 11:41:49 +0200

    style blog template

--- functions.php
+++ functions.php
@@ -1,29 +1,38 @@
-<?php 
+<?php
 
-function theme_enqueue_styles() {
+function theme_enqueue_styles()
+{
 
-// Get the theme data
-$the_theme = wp_get_theme();
-  wp_enqueue_style( 'mille-feuille-style', get_template_directory_uri() . '/assets/style.css', array(), $the_theme->get( 'Version' ) );
-  wp_enqueue_script( 'mille-feuille-script', get_template_directory_uri() . '/assets/app.js', array(), $the_theme->get( 'Version' ), true );
-  wp_enqueue_script( 'font-awesome', 'https://kit.fontawesome.com/41be139bfe.js', array(), $the_theme->get( 'Version' ), true );
+  // Get the theme data
+  $the_theme = wp_get_theme();
+  wp_enqueue_style('mille-feuille-style', get_template_directory_uri() . '/assets/style.css', array(), $the_theme->get('Version'));
+  wp_enqueue_script('mille-feuille-script', get_template_directory_uri() . '/assets/app.js', array(), $the_theme->get('Version'), true);
+  wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/41be139bfe.js', array(), $the_theme->get('Version'), true);
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
 // add footer and main menus
-function register_my_menu(){
-  register_nav_menu( 'main-menu', 'Menu principal' );
-  register_nav_menu( 'mobile-main-menu', 'Menu principal mobile' );
-  register_nav_menu( 'secondary-menu', 'Menu secondaire' );
-  register_nav_menu( 'footer', 'Footer' );
+function register_my_menu()
+{
+  register_nav_menu('main-menu', 'Menu principal');
+  register_nav_menu('mobile-main-menu', 'Menu principal mobile');
+  register_nav_menu('secondary-menu', 'Menu secondaire');
+  register_nav_menu('footer', 'Footer');
 }
-add_action( 'after_setup_theme', 'register_my_menu' );
+add_action('after_setup_theme', 'register_my_menu');
 
 // add custom logo feature
-add_theme_support( 'custom-logo' );
+add_theme_support('custom-logo');
 
 // change Fibosearch icon
-add_filter( 'dgwt/wcas/form/magnifier_ico', function ( $html, $class ) {
+add_filter('dgwt/wcas/form/magnifier_ico', function ($html, $class) {
   $html = '<i class="fa-solid fa-magnifying-glass ' . $class . '" aria-hidden="true"></i>';
   return $html;
-}, 10, 2 );
+}, 10, 2);
+
+// customize excerpt length
+function my_excerpt_length($length)
+{
+  return 30;
+}
+add_filter('excerpt_length', 'my_excerpt_length');
--- home.php
+++ home.php
@@ -1,23 +1,24 @@
 <?php get_header() ?>
 <main>
-  <div class="container">
-    <h1><?php single_post_title();; ?></h1>
+  <div class="container container--grid">
+    <h1 class="blog__title"><?php single_post_title();; ?></h1>
     <?php
     if (have_posts()) :
       while (have_posts()) :
         the_post(); ?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class($classes); ?>>
-          <div class="thumbnail">
-            <a href="<?php the_permalink(); ?>" class="thumbnail-link">
-              <?php the_post_thumbnail(); ?>
-            </a>
-          </div>
-          <header class="blog-entry-header clr">
-            <h2 class="blog-entry-title entry-title">
-              <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h2>
-          </header>
+          <?php if (get_the_post_thumbnail()) : ?>
+            <div class="post__image">
+              <a href="<?php the_permalink(); ?>" class="post__image-link">
+                <?php the_post_thumbnail(); ?>
+                <span class="post__image-overlay"></span>
+              </a>
+            </div>
+          <?php endif; ?>
+          <h2 class="post__title">
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+          </h2>
           <div class="post__date">
             <span class="screen-reader-text">Article publié le :</span>
             <i class="fa-solid fa-clock" aria-hidden="true"></i>
@@ -29,14 +30,22 @@
             echo $excerpt;
             ?>
           </div>
-          <div class="blog-entry-readmore clr">
+          <div class="post__readmore">
             <a href="<?php the_permalink(); ?>">Lire la suite<i class="fa fa-angle-right" aria-hidden="true"></i></a>
           </div>
         </article>
-    <?php
-      endwhile;
-    endif;
-    ?>
+      <?php endwhile; ?>
+      <div class="blog__pagination">
+        <?php
+        the_posts_pagination([
+          'prev_text' => '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+          'next_text' => '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+        ]);
+        ?>
+      </div>
+    <?php else : ?>
+      <p>Aucun article n'a été publié.</p>
+    <?php endif; ?>
   </div>
 </main>
 <?php get_footer() ?>
